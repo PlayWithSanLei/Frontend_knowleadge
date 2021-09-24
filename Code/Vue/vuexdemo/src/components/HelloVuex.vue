b460af872855bafd4a110c932241b3e1a0ea82fb
PlayWithSanLei <zhanghao63271202@gmail.com>
2021-09-24 18:25:33 +0800

    Vuex

--- Code/Vue/vuexdemo/src/components/HelloVuex.vue
+++ Code/Vue/vuexdemo/src/components/HelloVuex.vue
@@ -1,28 +1,49 @@
 <template>
   <div class="hello">
     helloVuex
-    <h2>{{$store.state.counter}}</h2>
-    <button @click="add()">+</button>
-    <button @click="sub()">-</button>
+    <h2>modules</h2>
+    <h2>{{$store.state.a.name}}</h2>
+    <button @click="updateModule"></button>
+    <h2>{{ $store.state.counter }}</h2>
+    <button @click="addCount(10)">+</button>
+    <button @click="subCount(5)">-</button>
+    <h2>平方</h2>
+    <h2>{{ $store.getters.powerCounter }}</h2>
+    <h2>{{ $store.getters.moreTWStudents }}</h2>
+    <h2>{{ $store.getters.getStudent }}</h2>
+    <h2>{{ $store.getters.moreAgeStudents(40) }}</h2>
+
+    <h2>{{$store.state.info}}</h2>
+    <button @click="updateInfo()">增加</button>
   </div>
 </template>
-
 <script>
 export default {
   name: "HelloVuex",
   methods: {
-    add() {
-      this.$store.commit('increment')
+    addCount(count) {
+      // this.$store.commit('incrCount', count)
+      this.$store.commit({
+        type: 'incrCount',
+        count: count
+      })
     },
-    sub() {
-      this.$store.commit('decrement')
+    subCount(count) {
+      this.$store.commit('decrCount', count)
+    },
+    updateInfo() {
+      this.$store.dispatch('aUpdateInfo')
+    },
+    updateModule() {
+      this.$store.commit('updateName')
     }
-  }
+  },
+  computed: {}
 }
 </script>
 
 <style scoped>
-  .hello {
-    border: 1px solid red;
-  }
+.hello {
+  border: 1px solid red;
+}
 </style>
--- Code/Vue/vuexdemo/src/store/index.js
+++ Code/Vue/vuexdemo/src/store/index.js
@@ -1,19 +1,73 @@
 import { createStore } from 'vuex'
+const moduleA = {
+  state: {
+    name: 'zhangsan'
+  },
+  mutations: {
+    updateName(state) {
+      state.name = 'lisi'
+    }
+  },
+  actions: {},
+  getters: {}
+}
 
 export default createStore({
   state: {
-    counter: 1000
+    counter: 1000,
+    students: [
+      {id: 110, name: 'why', age: 18},
+      {id: 111, name: 'James', age: 24},
+      {id: 112, name: 'kobe', age: 40},
+      {id: 113, name: 'curry', age: 10},
+    ],
+    info: {
+      name: 123,
+      age: 12,
+      zs: 'zs'
+    }
   },
-  mutations: {
+  mutations: { /*这里必须是同步方法*/
     increment(state) {
       state.counter++
     },
     decrement(state) {
       state.counter--
+    },
+    incrCount(state, payload) {
+      state.counter += payload.count
+    },
+    decrCount(state, count) {
+      state.counter -= count
+    },
+    updateInfo(state) {
+      state.info['sanlei'] = '123'
+    }
+  },
+  getters: {
+    powerCounter(state) {
+      return state.counter * state.counter
+    },
+    moreTWStudents(state) {
+      return state.students.filter(s => s.age >= 20)
+    },
+    getStudent(state, getters) {
+      return getters.moreTWStudents.length
+    },
+    moreAgeStudents(state) {
+      return age => {
+        return state.students.filter(s => s.age >= age)
+      }
     }
   },
   actions: {
+    aUpdateInfo(context) {
+      setTimeout(() => {
+        context.commit('updateInfo')
+      },1000)
+    }
   },
   modules: {
+    a: moduleA
   }
 })
